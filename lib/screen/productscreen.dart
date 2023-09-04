8ccd880a3011f1c709e2d74803bfb579a3283485
USER <lubnalubz5714@gmail.com>
2023-09-04 21:23:56 +0530

    project

--- lib/screen/productscreen.dart
+++ lib/screen/productscreen.dart
@@ -11,6 +11,28 @@ class ProductScreen extends StatefulWidget {
 class _ProductScreenState extends State<ProductScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.only(top: 50,left: 15,right: 15),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              InkWell(
+                onTap: (){
+                  Navigator.pop(context);
+                },
+                child: Icon(Icons.arrow_back,size: 30,),
+              ),
+              Text("Product",style: TextStyle(
+                fontSize: 25,
+                fontWeight: FontWeight.w500,
+              color: Colors.black54,),)
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
